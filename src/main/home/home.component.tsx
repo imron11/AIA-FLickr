f7bf69455d7fab7b192a9dd724df9999da2bfefe
imronimamwg <imronimam11@gmail.com>
2021-06-03 18:16:35 +0700

    setting up:  - layout design  - reusable component (logo, icon button)

--- src/main/home/home.component.tsx
+++ src/main/home/home.component.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { 
+  SafeAreaView, 
+  StyleSheet,  
+  View 
+} from "react-native";
 import { observer } from "mobx-react";
 import { container } from 'tsyringe';
 import HomeStore from './home.store';
 import colors from '../../shared/theme/colors';
+import LogoComponent from '../../shared/component/logo';
+import images from '../../asset/images';
+import icons from '../../asset/icons';
+import IconButtonComponent from '../../shared/component/button/icon.button';
 
 @observer
 class HomeComponent extends React.Component<any, any> {
@@ -14,13 +22,24 @@ class HomeComponent extends React.Component<any, any> {
     return(
       <SafeAreaView style={styles.mainContainer}>
         <View
-          style={styles.viewContainer}
+          style={styles.headerContainer}
         >
-          <Text
-            style={styles.nameText}
-          >
-            {`${this._homeStore.namePage}`}
-          </Text>
+          <LogoComponent
+            source={images.img_logo_flickr}
+          />
+
+          <IconButtonComponent
+            source={icons.ic_setting}
+            onPress={() => {
+              
+            }}
+          />
+        </View>
+
+        <View
+          style={styles.contentContainer}
+        >
+
         </View>
       </SafeAreaView>
     )
@@ -30,17 +49,20 @@ class HomeComponent extends React.Component<any, any> {
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    backgroundColor: colors.primary
+    backgroundColor: colors.flickrBlue
   },
-  viewContainer: {
-    flex: 1,
+  headerContainer: {
+    height: 100,
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    padding: 20
   },
-  nameText: {
-    color: 'darkred',
-    fontSize: 20,
-    fontWeight: '500'
+  contentContainer: {
+    flex: 1,
+    backgroundColor: colors.primary,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25
   }
 });
 
