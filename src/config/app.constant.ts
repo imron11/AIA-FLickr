eb67099067382a9b1d090de77a0a90034d0418c1
imronimamwg <imronimam11@gmail.com>
2021-06-03 16:51:57 +0700

    setting up API (base_url, api_key, secret)

--- src/config/app.constant.ts
+++ src/config/app.constant.ts
@@ -1,5 +1,7 @@
 const APP_CONSTANT = {
-  BASE_URL: ""
+  BASE_URL: "https://api.flickr.com/services/rest/?method=flickr.photos.search",
+  API_KEY: "a63320521223319663b7bf64a014626d",
+  API_SECRET: "104dd1d0b9f33340"
 }
 
 export default APP_CONSTANT;
