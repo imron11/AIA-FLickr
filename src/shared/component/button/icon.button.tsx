50919c55c56eb6ebedeeabee4136062ea4872a2b
imronimamwg <imronimam11@gmail.com>
2021-06-03 19:08:49 +0700

    adding props(style and height)

--- src/shared/component/button/icon.button.tsx
+++ src/shared/component/button/icon.button.tsx
@@ -8,7 +8,9 @@ interface iconButtonProps {
     onPress?: () => void;
     source: any;
     width?: number;
+    height?: number;
     tintColor?: any;
+    style?: any;
 }
 
 const IconButtonComponent = (props: iconButtonProps) => {
@@ -18,12 +20,14 @@ const IconButtonComponent = (props: iconButtonProps) => {
                 onPress={() => {
                     props.onPress();
                 }}
+                style={props.style}
             >
                 <Image
                     source={props.source}
-                    resizeMode={'contain'}
+                    resizeMode={'stretch'}
                     style={{
                         width: props.width ? props.width : 30,
+                        height: props.height ? props.height : 30,
                         tintColor: props.tintColor ? props.tintColor : "white"
                     }}
                 />
