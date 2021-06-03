dae2b791413485cae19670a632eb7b25a90a284a
imronimamwg <imronimam11@gmail.com>
2021-06-03 19:51:35 +0700

    adding format json

--- src/config/app.constant.ts
+++ src/config/app.constant.ts
@@ -1,7 +1,5 @@
 const APP_CONSTANT = {
-  BASE_URL: "https://api.flickr.com/services/rest/",
-  API_KEY: "a63320521223319663b7bf64a014626d",
-  API_SECRET: "104dd1d0b9f33340"
+  BASE_URL: "https://www.flickr.com/services/feeds/photos_public.gne/?format=json&nojsoncallback=1"
 }
 
 export default APP_CONSTANT;
