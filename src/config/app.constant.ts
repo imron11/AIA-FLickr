586cae41962074850fbd42fc9718fe1cac861b1c
imronimamwg <imronimam11@gmail.com>
2021-06-03 19:49:39 +0700

    changing baseurl to public feed

--- src/config/app.constant.ts
+++ src/config/app.constant.ts
@@ -1,5 +1,5 @@
 const APP_CONSTANT = {
-  BASE_URL: "https://api.flickr.com/services/rest/?method=flickr.photos.search",
+  BASE_URL: "https://api.flickr.com/services/rest/",
   API_KEY: "a63320521223319663b7bf64a014626d",
   API_SECRET: "104dd1d0b9f33340"
 }
